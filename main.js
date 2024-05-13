28841068725125fd2d0a9b58520d61b1c348c9c7
CullimoreMa550 <CullimoreMa550@northwested.org>
2024-05-13 10:33:45 -0400

    finished

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-// Student name
-// Current date
+// Maddox Cullimore
+// 13 May 2024
 // JS Arrow Functions Practice
 
 // View the markdown (.md) file in VS Code for instructions
+
+
+// const studentBio = (fName, lName, userAge) => `Hello! My name is ${fName} ${lName} and I am ${userAge} years old.`;
+
+// const firstName = 'Tania';
+// const lastName = 'Rascia';
+// const age = 17;
+// console.log(studentBio(firstName, lastName, age));
+
+
+// const welcomeMsg = (fName) => `Welcome to Career Tech, ${fName}!`;
+
+// const welcomeMsg = fName => `Welcome to Career Tech, ${fName}!`;
+
+// const firstName = 'Mikey';
+// console.log(welcomeMsg(firstName));
+
+
+
+const product = (num1, num2) => num1 * num2;
+
+
+const firstNum = 4;
+const secondNum = 5;
+console.log(`The product of ${firstNum} and ${secondNum} is: ${product(firstNum, secondNum)}`);
